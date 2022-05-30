1b703e79c699ca4381694983e4348d9e3163c2ff
marian-dub <m.dub@qarbon.it>
2022-05-30 16:59:47 +0200

    verbose for workspace entrypoint script

--- workspace/docker-entrypoint.sh
+++ workspace/docker-entrypoint.sh
@@ -4,6 +4,7 @@ CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
     # wait for mysql
+    echo "Waiting for MySQL connection"
     while ! mysqladmin ping -h"mysql" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --silent; do
       sleep 1
     done
@@ -15,6 +16,7 @@ if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
       make seed
     popd
 else
+    echo "No startup action"
     # script that should run the rest of the times
 fi
 
