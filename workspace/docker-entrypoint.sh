bcc0e090cc70d753acb9d55d8f0c831b0b8636f9
marian-dub <m.dub@qarbon.it>
2022-05-30 15:24:52 +0200

    make migration and seed db on container launch

---
+++ workspace/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# wait for mysql
+while ! mysqladmin ping -h"mysql" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --silent; do
+  sleep 1
+done
+
+# run artisan scripts
+pushd /var/www
+  composer install
+  make migrate
+  make seed
+popd
+
+# start workspace process
+/sbin/my_init
